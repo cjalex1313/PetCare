b838927d21249e2c73faae0a39e2fb1140512170
Alex Cioarec <cioarec.alexandru96@gmail.com>
2024-08-19 22:52:37 +0300

    Add mobile app skeleton

---
+++ PetCare.Mobile/petcare_mobile/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+final theme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: Color.fromARGB(255, 0, 46, 131),
+  ),
+  textTheme: GoogleFonts.latoTextTheme(),
+);
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: theme,
+      home: const Placeholder(),// Todo ...,
+    );
+  }
+}
