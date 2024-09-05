d6affd7f8e4d3042265654726570daa652d632e3
Isabela Cioarec <isabela@genesisdigital.co>
2024-09-05 23:40:09 +0300

    [main] Add route guards.

--- petcare-web/src/App.vue
+++ petcare-web/src/App.vue
@@ -1,18 +1,36 @@
 <script setup lang="ts">
 import Toast from 'primevue/toast';
-import { RouterLink, RouterView, useRouter } from 'vue-router'
-import { useUserStore } from './stores/user';
-import { onBeforeMount, onMounted } from 'vue';
+import { RouterView, useRouter } from 'vue-router'
+import { useUserStore } from '@/stores/user'
+import { onMounted } from 'vue';
+import Navigation from '@/components/Navigation.vue'
 
 const userStore = useUserStore();
+const router = useRouter();
 
 onMounted(() => {
   userStore.initialize();
 })
+router.beforeEach((to, from, next) => {
+  if (to.meta.forAuth) {
+    if (userStore.isLoggedIn) {
+      next();
+    } else {
+      next({ path: '/login' });
+    }
+  } else if (to.meta.forVisitors) {
+    if (!userStore.isLoggedIn) {
+      next();
+    } else {
+      next({ path: '/' });
+    }
+  }
+});
 </script>
 
 <template>
   <div>
+    <Navigation/>
     <RouterView />
     <Toast />
   </div>
