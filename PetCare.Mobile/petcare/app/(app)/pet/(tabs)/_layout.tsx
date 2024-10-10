724daae3238f3030d7a9e2cc5d5a574b478422ac
Alex Cioarec <cioarec.alexandru96@gmail.com>
2024-10-10 22:51:43 +0900

    Progress on mobile

--- PetCare.Mobile/petcare/app/(app)/pet/(tabs)/_layout.tsx
+++ PetCare.Mobile/petcare/app/(app)/pet/(tabs)/_layout.tsx
@@ -1,18 +1,36 @@
+import petApi from '@/api/petApi';
 import { Fontisto } from '@expo/vector-icons';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Stack, Tabs, useGlobalSearchParams, useNavigation } from 'expo-router';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function TabLayout() {
 
   const navigation = useNavigation();
-  const { id, name } = useGlobalSearchParams();
+  const isFocused = navigation.isFocused();
+  const { id } = useGlobalSearchParams();
+
+  const [name, setName] = useState<string>('');
 
   useEffect(() => {
     navigation.setOptions({
       title: name
     })
-  }, []);
+  }, [name]);
+
+  useEffect(() => {
+    if (isFocused) {
+      console.log('loads data')
+      loadData();
+    }
+  }, [isFocused])
+
+  const loadData = async () => {
+    const pet = await petApi.getPet(id as string);
+    if (pet) {
+      setName(pet.name);
+    }
+  }
 
   return (
     <Tabs screenOptions={{}}>
--- PetCare.Mobile/petcare/app/(app)/pet/(tabs)/index.tsx
+++ PetCare.Mobile/petcare/app/(app)/pet/(tabs)/index.tsx
@@ -1,14 +1,15 @@
 import petApi from '@/api/petApi';
 import { PetDTO } from '@/api/types/pets';
 import { displayPetSex, displayPetType } from '@/utils/petUtils';
-import { Stack, useGlobalSearchParams, useNavigation } from 'expo-router';
+import { Stack, useGlobalSearchParams, useNavigation, useRouter } from 'expo-router';
 import { useEffect, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import { List } from 'react-native-paper';
+import { Button, List } from 'react-native-paper';
 
 export default function Tab() {
 
   const { id, petType } = useGlobalSearchParams();
+  const router = useRouter();
 
   const [pet, setPet] = useState<PetDTO | undefined>();
 
@@ -21,6 +22,13 @@ export default function Tab() {
     setPet(petResponse);
   }
 
+  const editPet = () => {
+    console.log(1);
+    router.push({
+      pathname: '/(app)/pet/petEdit'
+    })
+  }
+
   return (
     <View style={styles.container}>
       {pet && <List.Section>
@@ -30,6 +38,9 @@ export default function Tab() {
         <List.Item title="Sex" description={displayPetSex(pet.sex)} />
         <List.Item title="Date of Birth" description={pet.dateOfBirth.toLocaleDateString()} />
       </List.Section>}
+      <Button mode="contained" onPress={editPet} style={styles.button}>
+        Edit
+      </Button>
     </View>
   );
 }
@@ -38,4 +49,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  button: {
+    margin: 6
+  }
 });
