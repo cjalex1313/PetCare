f7468d1b587c3ff9a186d54898bdd3351c2c4bad
Alex Cioarec <cioarec.alexandru96@gmail.com>
2024-10-19 00:06:40 +0300

    Add healthcheck controller

---
+++ PetCare.Server/Controllers/HealthChecksController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PetCare.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [AllowAnonymous]
+    [ApiController]
+    public class HealthChecksController : ControllerBase
+    {
+        [HttpGet("Ping")]
+        public IActionResult Ping()
+        {
+            return Ok(new
+            {
+                DateTime = DateTime.UtcNow,
+            });
+        }
+    }
+}
