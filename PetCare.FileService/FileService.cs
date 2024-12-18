50f0883e2acc49b1e818b7d969469347171f1344
Alex Cioarec <cioarec.alexandru96@gmail.com>
2024-12-18 03:03:13 +0200

    Add support for s3 files

--- PetCare.FileService/FileService.cs
+++ PetCare.FileService/FileService.cs
@@ -7,6 +7,8 @@ namespace PetCare.FileService;
 public interface IFileService
 {
     Task UploadFile(byte[] fileBytes, string extension, string key);
+    Task<byte[]> DownloadFile(string key);
+    string GetFileUrl(string key);
 }
 
 public class FileService : IFileService 
@@ -19,8 +21,8 @@ public class FileService : IFileService
         var s3Config = new AmazonS3Config();
         s3Config.ServiceURL = config.ServiceUrl;
         s3Config.ForcePathStyle = true;
-        _client = new AmazonS3Client(_config!.SpacesKey, _config.SpacesSecret, s3Config);
         _config = config;
+        _client = new AmazonS3Client(_config!.SpacesKey, _config.SpacesSecret, s3Config);
     }
 
     public async Task UploadFile(byte[] fileBytes, string extension, string key)
@@ -31,8 +33,31 @@ public class FileService : IFileService
             Key = $"{key}.{extension}",
             InputStream = new MemoryStream(fileBytes),
             ContentType = "application/octet-stream",
-            AutoCloseStream = true
+            AutoCloseStream = true,
+            CannedACL = S3CannedACL.PublicRead
         };
         await _client.PutObjectAsync(putObjectRequest);
+    }
+
+    public async Task<byte[]> DownloadFile(string key)
+    {
+        var getObjectRequest = new GetObjectRequest()
+        {
+            BucketName = _config.BucketName,
+            Key = key
+        };
+        var response = await _client.GetObjectAsync(getObjectRequest);
+        byte[] fileBytes;
+        using (var memoryStream = new MemoryStream())
+        {
+            await response.ResponseStream.CopyToAsync(memoryStream);
+            fileBytes = memoryStream.ToArray();
+        }
+        return fileBytes;
+    }
+
+    public string GetFileUrl(string key)
+    {
+        return $"{_config.ServiceUrl}/{_config.BucketName}/{key}";
     }
 }
--- PetCare.Server/Controllers/WeatherForecastController.cs
+++ PetCare.Server/Controllers/WeatherForecastController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using PetCare.FileService;
 using PetCare.Shared.Common;
 
 namespace PetCare.Server.Controllers
@@ -8,10 +9,17 @@ namespace PetCare.Server.Controllers
     [Route("api/[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+        private readonly IFileService _fileService;
+
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
+
+        public WeatherForecastController(IFileService fileService)
+        {
+            _fileService = fileService;
+        }
 
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
@@ -36,6 +44,49 @@ namespace PetCare.Server.Controllers
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+        }
+
+        [HttpPost("TestGetFile")]
+        public async Task<IActionResult> TestGetFile([FromBody] string key)
+        {
+            var bytes = await _fileService.DownloadFile(key);
+            return Ok(bytes);
+        }
+
+        [HttpPost("GetFileUrl")]
+        public IActionResult GetFileUrl([FromBody] string key)
+        {
+            var fileUrl = _fileService.GetFileUrl(key);
+            return Ok(fileUrl);
+        }
+
+        [HttpPost("TestFileUpload")]
+        public async Task<IActionResult>  TestFileUpload(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+            {
+                return BadRequest("No file was uploaded.");
+            }
+
+            // Get file name and extension
+            var fileName = file.FileName;
+            var fileExtension = Path.GetExtension(fileName).Substring(1);
+
+            // Read file into a byte array
+            byte[] fileBytes;
+            using (var memoryStream = new MemoryStream())
+            {
+                await file.CopyToAsync(memoryStream);
+                fileBytes = memoryStream.ToArray();
+            }
+            var id = Guid.NewGuid();
+            await _fileService.UploadFile(fileBytes, fileExtension, id.ToString());
+            // Example: Return file details for testing purposes
+            return Ok(new
+            {
+                FileName = fileName,
+                Id = id
+            });
         }
     }
 }
